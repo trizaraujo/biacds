c426361300acdc40ebb4c4ade33575cb53f8a9a3
Beatriz Araujo <sousabeatriz234@gmail.com>
2024-03-26 19:52:54 -0300

    apostila(git-java): calculo de soma e subtracao implementados

---
+++ Calculadora.java
@@ -0,0 +1,9 @@
+public class Calculadora {
+
+    public double somar (double a, double b){
+        return a + b;
+    }
+    public double subtrair (double a, double b){
+        return a - b;
+    }
+}
